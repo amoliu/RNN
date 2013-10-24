4724b3a0ca83881f07a24efe5f8847549a0e1513
Jeremy Nash <jeremynash@67-194-8-6.wireless.umnet.umich.edu>
2013-10-24 17:18:43 -0400

    Added an LSTM folder

---
+++ LSTM/LSTM.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+class LSTM:
+    def __init__(self):
+        pass
+
+    def setInput(self, x):
+        pass
+
+def main():
+    lstm = LSTM()
+    
+if __name__ == "__main__":
+    main()
